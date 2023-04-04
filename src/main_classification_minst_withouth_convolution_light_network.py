26e4e834ca790612aea1b384959ae148774bbe6a
dasilvar <remi.dasilva@edu.esiee.fr>
2023-04-04 18:32:51 +0200

    a

--- src/main_classification_minst_withouth_convolution_light_network.py
+++ src/main_classification_minst_withouth_convolution_light_network.py
@@ -1,13 +1,19 @@
-import network
+import network2
 import numpy as np
-import matplotlib.pyplot as plt
 import mnist_loader
+import matplotlib.pyplot as plt
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net = network2.Network([784], cost=network2.CrossEntropyCost)
+net.SGD(training_data, 
+    epochs=30, 
+    mini_batch_size=10, 
+    eta=0.1,
+    lmbda=5.0,
+    evaluation_data=validation_data,
+    monitor_evaluation_accuracy=True)
 
 validation_data = list(validation_data)
 test_data = list(test_data)
